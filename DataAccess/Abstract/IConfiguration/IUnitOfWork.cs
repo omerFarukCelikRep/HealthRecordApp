b3d4fbc9d3e0a2f60972aef5a72074674277d0ee
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2021-12-11 00:23:34 +0300

    IHealthDataRepository property added

--- DataAccess/Abstract/IConfiguration/IUnitOfWork.cs
+++ DataAccess/Abstract/IConfiguration/IUnitOfWork.cs
@@ -7,6 +7,7 @@ namespace DataAccess.Abstract.IConfiguration
     {
         IUsersRepository Users { get; }
         IRefreshTokenRepository RefreshTokens { get; }
+        IHealthDataRepository HealthDatas { get; }
         Task CompleteAsync();
     }
 }
