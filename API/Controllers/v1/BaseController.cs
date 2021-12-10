fbbb68a930227e5b98dd4c2c8cd944255f3a7cbd
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2021-12-11 00:15:21 +0300

    IMapper and PopulateError method added

--- API/Controllers/v1/BaseController.cs
+++ API/Controllers/v1/BaseController.cs
@@ -1,4 +1,6 @@
-﻿using DataAccess.Abstract.IConfiguration;
+﻿using AutoMapper;
+using DataAccess.Abstract.IConfiguration;
+using Entity.Dtos.Errors;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,10 +13,22 @@ public class BaseController : ControllerBase
 {
     protected readonly IUnitOfWork _unitOfWork;
     public UserManager<IdentityUser> _userManager;
+    public readonly IMapper _mapper;
 
-    public BaseController(IUnitOfWork unitOfWork, UserManager<IdentityUser> userManager)
+    public BaseController(IUnitOfWork unitOfWork, UserManager<IdentityUser> userManager, IMapper mapper)
     {
         _unitOfWork = unitOfWork;
         _userManager = userManager;
+        _mapper = mapper;
+    }
+
+    internal Error PopulateError(int code, string message,string type)
+    {
+        return new Error()
+        {
+            Code = code,
+            Message = message,
+            Type = type
+        };
     }
 }
