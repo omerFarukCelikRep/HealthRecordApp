866d982ad3414163ce4f718b99bcff8becfee744
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2021-12-11 00:26:37 +0300

    Generic Result class created

---
+++ Entity/Dtos/Generic/Result.cs
@@ -0,0 +1,13 @@
+﻿using Entity.Dtos.Errors;
+using System;
+
+namespace Entity.Dtos.Generic
+{
+    public class Result<T> //Single Item return
+    {
+        public T Content { get; set; }
+        public Error Error { get; set; }
+        public bool IsSuccess => Error == null;
+        public DateTime ResponseTime { get; set; } = DateTime.UtcNow;
+    }
+}
