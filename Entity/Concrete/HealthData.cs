45299e2371feba7f4831441fe9a83dc8e5387f36
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2021-12-11 00:25:22 +0300

    HealtData entity class created

---
+++ Entity/Concrete/HealthData.cs
@@ -0,0 +1,18 @@
+﻿using Entity.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class HealthData : BaseEntity
+    {
+        public decimal Height { get; set; }
+        public decimal Weight { get; set; }
+        public string BloodType { get; set; } //TODO: Make This Information based on enum
+        public string Race { get; set; }
+        public bool UseGlasses { get; set; }
+    }
+}
