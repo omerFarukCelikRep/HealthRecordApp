02431a2512f599cd983790c548c30d7cdeadc1a1
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2021-12-11 00:24:07 +0300

    HealthData Dbset added

--- DataAccess/EntityFramework/Context/AppDbContext.cs
+++ DataAccess/EntityFramework/Context/AppDbContext.cs
@@ -8,6 +8,7 @@ namespace DataAccess.EntityFramework.Context
     {
         public virtual DbSet<AppUser> AppUsers { get; set; }
         public virtual DbSet<RefreshToken> RefreshTokens { get; set; }
+        public virtual DbSet<HealthData> HealthDatas { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
