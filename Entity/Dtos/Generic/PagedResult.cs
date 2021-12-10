b765479a17f325374d321e18112241909032d364
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2021-12-11 00:26:09 +0300

    Generic PagedResult class created as result class

---
+++ Entity/Dtos/Generic/PagedResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Entity.Dtos.Generic
+{
+    public class PagedResult<T> : Result<List<T>>
+    {
+        public int Page { get; set; }
+        public int ResultCount { get; set; }
+        public int ResultsPerPage { get; set; }
+    }
+}
