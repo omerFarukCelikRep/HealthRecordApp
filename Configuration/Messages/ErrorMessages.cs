79c6769f5cbd77932c520e041e57367ae4371521
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2021-12-11 00:22:44 +0300

    ErrorMessages class created

---
+++ Configuration/Messages/ErrorMessages.cs
@@ -0,0 +1,25 @@
+﻿namespace Configuration.Messages
+{
+    public static class ErrorMessages
+    {
+        public static class Generic
+        {
+            public static string SomethingWentWrong = "Something went wrong, please try again later";
+            public static string UnableToProcess = "Unable to process request";
+            public static string TypeBadRequest = "Bad Request";
+            public static string InvalidPayload = "Invalid Payload";
+            public static string InvalidRequest = "Invalid Request";
+            public static string ObjectNotFound = "Object Not Found";
+        }
+
+        public static class Profile
+        {
+            public static string UserNotFound = "User Not Found";
+        }
+
+        public static class Users
+        {
+            public static string UserNotFound = "User Not Found";
+        }
+    }
+}
